7ff13079adfaba959d169b84142cf71d016152dc
Ashish Shukla <akshkgd@gmail.com>
2022-01-12 15:05:56 +0530

    code added

---
+++ intro to js/script.js
@@ -0,0 +1,38 @@
+// ES6 ->  
+// variables and data types
+// variables -> temp. container to store data
+
+// let and const > var??
+ 
+
+// let name = 'ashish shukla';
+// console.log(name);
+
+
+// const userId = 1234;
+// console.log(userId);
+
+
+// var userName = 'ashish';
+// console.log(userName);
+
+// Data types => strings, numbers, booleans, array,  null, undefined, , object
+
+let name = 'ashish';
+console.log( name);
+
+let age = 20.45;
+console.log(typeof age);
+
+
+let isAlive = false;
+console.log(typeof isAlive);
+
+let hobbies = ['cricket', 'football', 'coding', 'reading', 'writing', 'cooking'];
+console.log(hobbies[3]);
+
+
+
+
+
+
