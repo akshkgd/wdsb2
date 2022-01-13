2cb336fe35a7bded786bf1f11d5908b64f044842
Ashish Shukla <akshkgd@gmail.com>
2022-01-13 18:21:34 +0530

    functions

---
+++ intro to js/conditionals.js
@@ -0,0 +1,43 @@
+// let wether = 'cold';
+// // prompt -> returns a string
+// console.log('The weather is ', wether);
+// if (wether === 'rainy') {
+//     console.log('bring umbrella');
+// }
+// else if (wether === 'sunny') {
+//     console.log('take the sunglasses');
+// }
+// else if(wether === 'cold') {
+//     console.log('take the coat');
+// }
+// else {
+//     console.log('Good to go...');
+// }
+
+// let number1 = parseInt(prompt("Enter the first number")); // '10'
+// let number2 = parseInt(prompt("Enter the second number")); // '10'
+
+// console.log(number1 + number2);
+
+//check if eligible for voting
+
+// let name = prompt("Enter your name");
+// let age = parseInt(prompt("Enter your age"));
+
+// if(age >= 18) {
+//     console.log(name, 'is eligible for voting');
+// }
+// else {
+//     console.log(name, 'is not eligible for voting');
+// }
+
+//check if even or odd
+
+let number = parseInt(prompt("Enter a number"));
+
+if(number % 2 === 0) {
+    document.write(number, 'is even');
+}
+else {
+    document.write(number, 'is odd');
+}
---
+++ intro to js/functions.js
@@ -0,0 +1,34 @@
+// function grinder(item){
+//     console.log("Grinding...", item);
+// }
+
+// let items = 'cofee beans';
+// grinder(items);
+// grinder('potatoes');
+
+
+function calculateTds(salary){
+    return salary * 0.2;
+}
+
+let salary = 100000;
+// let tds = calculateTds(salary);
+console.log(calculateTds(salary));
+
+// console.log(salary-tds);
+
+
+// arrow function -> es6
+
+function demo(){
+    // syntax of function
+}
+
+demo = () => {
+    // syntax of arrow function
+}
+
+cube = (number) => number * number * number; //arrow function
+
+
+console.log(cube(5));
